133df4830ce69ad5d9d3d7a8c35222e73f64b2b1
Woo Jin Park <woojinp@gmail.com>
2013-06-03 18:19:19 -0700

    set up gogomuni rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gogomuni::Application.config.secret_token = '1762b24853182535d86576689701ba4b6f63d9954eeaaa47aa3ad0e7ab560825f28b4eb3838f412bb887ce9fbff6f306ff352307f3cd52d040ee96337b62da22'
